2027d33827c07b3c6d56557fbd90aa882d9755cc
Wilfred <wbridges905@gmail.com>
2023-09-16 13:36:12 +0100

    html starting code and file setup

---
+++ task/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(window).on('load', function () {
+        if ($('#preloader').length) {
+            $('#preloader').delay(1000).fadeOut('slow', function () {
+                $(this).remove();
+            });
+        }
+    });
+});
