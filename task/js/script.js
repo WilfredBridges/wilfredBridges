2ee1651e7599a98cd0bda34b1ccd63833fd2d2b8
Wilfred <wbridges905@gmail.com>
2023-09-20 14:08:03 +0100

    added regions tab and functionality

--- task/js/script.js
+++ task/js/script.js
@@ -1,25 +1,38 @@
 $(document).ready(function () {
-    $('#countryForm').submit(function (e) {
-        e.preventDefault();
-        var country = $('#countryInput').val();
-
-        // First, get the geonameId
+    // Function to fetch Geoname ID based on country name
+    function fetchGeonameId(countryName, callback) {
         $.ajax({
             type: 'POST',
             url: 'get_geoname_id.php',
-            data: { country: country, wilfredbridges: 'wilfredbridges' },
+            data: { country: countryName, wilfredbridges: 'wilfredbridges' },
             success: function (data) {
                 console.log(data);
                 var geonameData = JSON.parse(data);
                 if (geonameData.geonameId) {
-                    fetchNeighbours(geonameData.geonameId);
+                    callback(geonameData.geonameId);
                 } else {
                     $('#results').html('Error: GeonameId not found.');
                 }
             },
             error: function () {
-                $('#results').html('Error fetching geonameId.');
+                $('#results').html('Error fetching GeonameId.');
             }
+        });
+    }
+
+    // First API call (neighboring countries)
+    $('#fetchDataButton').click(function () {
+        var countryName = $('#countryInput').val();
+        fetchGeonameId(countryName, function (geonameId) {
+            fetchNeighbours(geonameId);
+        });
+    });
+
+    // Second API call (country regions)
+    $('#fetchCountryRegionsButton').click(function () {
+        var countryName = $('#countryInput2').val();
+        fetchGeonameId(countryName, function (geonameId) {
+            fetchCountryChildren(geonameId);
         });
     });
 
@@ -39,25 +52,59 @@ $(document).ready(function () {
         });
     }
 
+    function fetchCountryChildren(geonameId) {
+        // Fetch children (regions) based on the Geoname ID
+        $.ajax({
+            type: 'POST',
+            url: 'php/get_country_children.php',
+            data: { geonameId: geonameId, wilfredbridges: 'wilfredbridges' },
+            success: function (data) {
+                console.log(data);
+                displayCountryRegionsResults(data);
+            },
+            error: function () {
+                $('#countryRegionsResults').html('Error fetching data.');
+            }
+        });
+    }
+
     function displayResults(data) {
         var resultHtml = '';
         var response = JSON.parse(data);
-    
+
         if (response.totalResultsCount > 0) {
             $.each(response.geonames, function (index, neighbor) {
                 resultHtml += '<tr>';
                 resultHtml += '<td>' + neighbor.asciiName + '</td>';
-                resultHtml += '<td>' + neighbor.countryName + '</td>';
+                resultHtml += '<td>' + '(' + neighbor.countryName + ')' + '</td>';
                 resultHtml += '<td></td>';
                 resultHtml += '</tr>';
             });
         } else {
             resultHtml = '<tr><td colspan="3">No data available.</td></tr>';
         }
-    
+
         $('#results').html(resultHtml);
     }
-    
+
+    function displayCountryRegionsResults(data) {
+        var resultHtml = '';
+        var response = JSON.parse(data);
+
+        if (response.totalResultsCount > 0) {
+            $.each(response.geonames, function (index, region) {
+                resultHtml += '<tr>';
+                resultHtml += '<td>' + region.toponymName + '</td>';
+                resultHtml += '<td>' + region.countryName + '</td>';
+                resultHtml += '<td></td>';
+                resultHtml += '</tr>';
+            });
+        } else {
+            resultHtml = '<tr><td colspan="3">No data available.</td></tr>';
+        }
+
+        $('#countryRegionsResults').html(resultHtml);
+    }
 
     // Preloader
     $(window).on('load', function () {
